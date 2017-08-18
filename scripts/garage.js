2b2ad4c7d6d3bdae115ea2cc945a4c7b371d8918
devjanaprime <dev@primeacademy.io>
2017-08-18 01:12:17 -0500

    added DOM manip

--- scripts/garage.js
+++ scripts/garage.js
@@ -4,14 +4,28 @@ let garage = [];
 let addCar = () => {
   let newCar = new Car( document.getElementById( 'yearIn' ).value,
     document.getElementById( 'makeIn' ).value,
-    document.getElementById( 'modelIn' ).value);
+    document.getElementById( 'modelIn' ).value,
+    document.getElementById( 'imageUrlIn' ).value,
+   );
   garage.push( newCar );
+  displayCars();
 }; // end add car
 
+let displayCars = () => {
+  let outputText = '';
+  for( let i=0; i< garage.length; i++ ){
+    outputText += '<div><p>' + garage[i].year + ' ' + garage[i].make + ' ' + garage[i].model + '</p>';
+    outputText += '<img src="' + garage[i].imageUrl + '"></div>';
+  } // end for
+  let output = document.getElementById( 'output' );
+  output.innerHTML = outputText;
+}; //end displayCars
+
 class Car{
-  constructor( year, make, model ){
+  constructor( year, make, model, imageUrl ){
    this.year = year;
    this.make = make;
    this.model = model;
+   this.imageUrl = imageUrl;
  }; // end car constructor
 }; //end Car class
